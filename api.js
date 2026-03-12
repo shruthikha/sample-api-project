33853e0018c9293de3813b88437585abbb9324ee
shruthikha <shruthisuresh0317@gmail.com>
2026-03-12 12:55:50 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,2 +1,20 @@
-// trigger workflow 
-There is no backend source code provided to analyze. Please provide the backend source code so I can extract the API information and generate the documentation accordingly.
+const express = require("express");
+const app = express();
+
+app.get("/users", (req, res) => {
+  res.json({ users: [] });
+});
+
+app.post("/users", (req, res) => {
+  res.json({ message: "User created" });
+});
+
+app.put("/users/:id", (req, res) => {
+  res.json({ message: "User updated" });
+});
+
+app.delete("/users/:id", (req, res) => {
+  res.json({ message: "User deleted" });
+});
+
+module.exports = app;
